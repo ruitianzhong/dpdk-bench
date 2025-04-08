86c84ceaea82b51a85f4fe83d9395dac36871d30
ruitianzhong <zhongruitian2003@qq.com>
2025-04-08 16:04:20 +0000

    add ablation script

--- scripts/run_evaluation.py
+++ scripts/run_evaluation.py
@@ -6,15 +6,22 @@ from matplotlib import pyplot as plt
 from run_motivation import write_json
 import argparse
 
-def run_back2back(app_name, gbps, enable_aggregator):
+
+def run_back2back(app_name, gbps, enable_aggregator, enable_ablation=0, delay_cycle=0, access_byte_per_packet=0):
+    # reasonable offered load
     assert (1 <= gbps <= 40)
 
+    assert (not (delay_cycle != 0 and access_byte_per_packet != 0))
+
     cmdline = f"./aggregator/build/back2back --  " \
-        " --app chain " \
+        f" --app {app_name} " \
         " --pcap_file ./synthetic_flow_num10000_seed42.pcap " \
         " --fw_rules ./fw-testing-10000.rules " \
         f" --enable-aggregator {1 if enable_aggregator else 0} " \
-        f" --gbps {str(gbps)}"
+        f" --gbps {str(gbps)} " \
+        f" --ablation {1 if enable_ablation else 0} "   \
+        f" --delay-cycle {delay_cycle} "    \
+        f" --access-byte-per-packet {access_byte_per_packet} "
 
     print(cmdline)
     cmdline = cmdline.split()
@@ -27,7 +34,7 @@ def run_back2back(app_name, gbps, enable_aggregator):
         for _ in range(0, repeat):
 
             result = subprocess.run(cmdline, check=True,
-                                capture_output=True, text=True)
+                                    capture_output=True, text=True)
             # print(result)
             lines = result.stdout.split("\n")
             for line in lines:
@@ -93,7 +100,7 @@ def generate_latency_figure(with_agg_results, without_agg_results):
     with_agg_y = [result[1]['latency'] for result in with_agg_results]
     without_agg_y = [result[1]['latency'] for result in without_agg_results]
     print_figure(x, with_agg_y, "w/ aggregator", without_agg_y, 'w/o aggregator', '',
-                 'Offered Load (Gbps)', '', 'latency.png')
+                 'Offered Load (Gbps)', 'Average Latency Per Packet (us)', 'latency.png')
 
 
 def generate_cycle_figure(with_agg_results, without_agg_results):
@@ -104,14 +111,94 @@ def generate_cycle_figure(with_agg_results, without_agg_results):
                  'Offered Load (Gbps)', 'Average CPU Cycles Per Packet', 'cycle.png')
 
 
-def generate_back2back_figure():
-    pass
-
 def do_preparation():
-    cmdline= "./scripts/generate_testing_flow"
+    cmdline = "./scripts/generate_testing_flow"
     cmdline = cmdline.split()
     subprocess.run(cmdline, check=True)
     print("Preparation done!")
+
+
+def do_miss_penalty_ablation():
+    with_agg = []
+    without_agg = []
+    for miss_penalty_cycle in range(100, 1700, 100):
+       ret = run_back2back(app_name="chain", gbps=30, enable_aggregator=True,
+                           enable_ablation=True, delay_cycle=miss_penalty_cycle)
+
+       with_agg.append(ret)
+       ret = run_back2back(app_name="chain", gbps=30, enable_aggregator=False,
+                           enable_ablation=True, delay_cycle=miss_penalty_cycle)
+       without_agg.append(ret)
+
+    x = range(100, 1700, 100)
+    # cycle
+    y1 = [e['cycle'] for e in with_agg]
+    y2 = [e['cycle'] for e in without_agg]
+    prefix = "ablation_miss_penalty"
+
+    print_figure(x, y1, 'w/ aggregator', y2, 'w/o aggregator', '',
+                 'Miss Penalty (cycle)', 'Average CPU Cycle Per Packet', prefix + '_cycle.png')
+
+    #  latency
+    y1 = [e['latency'] for e in with_agg]
+    y2 = [e['latency'] for e in without_agg]
+
+    print_figure(x, y1, 'w/ aggregator', y2, 'w/o aggregator', '',
+                 'Miss Penalty (cycle)', 'Average Latency Per Packet (us)', prefix+'_latency.png')
+
+    y1 = [e['throughput'] for e in with_agg]
+    y2 = [e['throughput'] for e in without_agg]
+
+    print_figure(x, y1, 'w/ aggregator', y2, 'w/o aggregator', '',
+                 'Miss Penalty (cycle)', 'Throughput (Gbps)', prefix+'_throughput.png')
+
+    write_json(prefix+'_with_agg.json', with_agg)
+    write_json(prefix+'_without_agg.json', without_agg)
+
+
+def do_cache_ablation():
+    with_agg = []
+    without_agg = []
+    x = range(32, 1024, 32)
+    for access_mem_size in x:
+       ret = run_back2back(app_name="chain", gbps=30, enable_aggregator=True,
+                           enable_ablation=True, access_byte_per_packet=access_mem_size)
+
+       with_agg.append(ret)
+       ret = run_back2back(app_name="chain", gbps=30, enable_aggregator=False,
+                           enable_ablation=True, access_byte_per_packet=access_mem_size)
+       without_agg.append(ret)
+
+    # cycle
+    y1 = [e['cycle'] for e in with_agg]
+    y2 = [e['cycle'] for e in without_agg]
+    prefix = "ablation_mem_access"
+    x_axis_name = "Memory Access Per Packet (byte)"
+
+    print_figure(x, y1, 'w/ aggregator', y2, 'w/o aggregator', '',
+                 x_axis_name, 'Average CPU Cycle Per Packet', prefix + '_cycle.png')
+
+    #  latency
+    y1 = [e['latency'] for e in with_agg]
+    y2 = [e['latency'] for e in without_agg]
+
+    print_figure(x, y1, 'w/ aggregator', y2, 'w/o aggregator', '',
+                 x_axis_name, 'Average Latency Per Packet (us)', prefix+'_latency.png')
+
+    y1 = [e['throughput'] for e in with_agg]
+    y2 = [e['throughput'] for e in without_agg]
+
+    print_figure(x, y1, 'w/ aggregator', y2, 'w/o aggregator', '',
+                 x_axis_name, 'Throughput (Gbps)', prefix+'_throughput.png')
+
+    write_json(prefix+'_with_agg.json', with_agg)
+    write_json(prefix+'_without_agg.json', without_agg)
+
+
+def do_ablation():
+    do_miss_penalty_ablation()
+    do_cache_ablation()
+
 
 def do_evaluation():
     print(run_back2back("chain", 25, True))
@@ -133,13 +220,17 @@ def do_evaluation():
     write_json("eval_with_agg.json", with_agg_results)
     write_json("eval_without_agg.json", without_agg_results)
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--prepare", action="store_true", default=False)
     parser.add_argument("--run", action="store_true", default=False)
+    parser.add_argument("--ablation", action="store_true", default=False)
     args = parser.parse_args()
     if args.prepare:
         do_preparation()
     if args.run:
         do_evaluation()
+    if args.ablation:
+        do_ablation()
